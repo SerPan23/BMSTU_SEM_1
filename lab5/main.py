8d0429db3d7b3d5b528c2f5a5f99e7d2b6150398
Sergei <igracom@yandex.ru>
2022-10-11 11:24:20 +0300

    Update main.py

--- lab5/main.py
+++ lab5/main.py
@@ -5,6 +5,8 @@
 # z = 1 - 1/2 * x + ... + (-1)**n * (1*3*...*(2n-1)) * x**n / (2*4*...*(2*n))
 # с точностью до члена ряда ε
 
+import math as m
+
 x = float(input('Введите значение аргумента: '))
 eps = float(input('Введите точность: '))
 
@@ -66,9 +68,19 @@ else:
         for j in range(2, 2 * i + 1, 2):
             denominator *= j
 
-        current = (-1) ** i * (numerator / denominator) * x ** i
+        # замена степени на умножение, чтобы проверить переполнение
+        tmp = x
+        for j in range(i):
+            tmp *= x
+
+        # current = (-1) ** i * (numerator / denominator) * x ** i
+        current = (-1) ** i * (numerator / denominator) * tmp
 
         z += current
+
+        if m.isinf(z):
+            finish_i = -2
+            break
 
         if i % step == 0:
             print(f'| {i + 1:^12.5g} | {current:^12.5g} | {z:^12.5g} |')
@@ -79,7 +91,9 @@ else:
 
     print('-' * table_width)
 
-    if finish_i != -1:
+    if finish_i == -2:
+        print("Ошибка! Переполнение, указанной точности достичь не удалось")
+    elif finish_i != -1:
         print(f'Сумма бесконечного ряда - {z:^.5g}, вычислена за {finish_i:^.5g} итерацию(й).')
     else:
         print('За указанное число итераций необходимой точности достичь не удалось.')

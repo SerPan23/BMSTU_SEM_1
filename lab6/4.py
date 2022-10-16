a64e9ef351197f7b163f2a90df65877db91f95bb
Sergei <igracom@yandex.ru>
2022-10-16 19:52:27 +0300

    init lab6

--- lab6/4.py
+++ lab6/4.py
@@ -1,3 +1,51 @@
 # Паншин Сергей ИУ7-13Б
 # Найти наиболее длинную непрерывную последовательность
 # возрастающих простых чисел
+
+n = int(input('Введите количество элементов списка: '))
+
+while n <= 0:
+    print('Ошибка! Количество элементов должно быть больше 0')
+    n = int(input('Введите количество элементов списка: '))
+
+array = [0] * n
+
+for i in range(n):
+    array[i] = int(input(f'Введите {i + 1} элемент списка: '))
+
+tmp = []
+max_seq = []
+f = True
+for i in range(n):
+    flag = True
+
+    for j in range(2, array[i] // 2 + 1):
+        if array[i] % j == 0:
+            flag = False
+            break
+
+    if f and flag:
+        f = False
+        tmp.append(array[i])
+    else:
+        if array[i - 1] < array[i] and flag:
+            tmp.append(array[i])
+            if len(tmp) > len(max_seq):
+                max_seq = tmp
+        else:
+            if flag:
+                tmp = [array[i]]
+            else:
+                f = True
+                tmp = []
+
+if len(tmp) > len(max_seq):
+    max_seq = tmp
+
+if len(max_seq) == 0:
+    print('Последовательность с данными критериями не найдена')
+else:
+    print(f'Длинна наиболее длинной последовательности: {len(max_seq)}')
+    print('Элементы найденной последовательности:')
+    for i in range(len(max_seq)):
+        print(f'{i+1} элемент: {max_seq[i]}')
--- lab6/5.py
+++ lab6/5.py
@@ -1,2 +1,32 @@
 # Паншин Сергей ИУ7-13Б
 # Поменять местами элементы последний нулевой и максимальный отрицательный
+
+n = int(input('Введите количество элементов списка: '))
+
+while n <= 0:
+    print('Ошибка! Количество элементов должно быть больше 0')
+    n = int(input('Введите количество элементов списка: '))
+
+array = [0] * n
+
+negative_nums = []
+last_0_id = -1
+
+for i in range(n):
+    array[i] = int(input(f'Введите {i + 1} элемент списка: '))
+
+    if array[i] < 0:
+        negative_nums.append(array[i])
+
+    if array[i] == 0:
+        last_0_id = i
+
+if last_0_id == -1:
+    print('Ошибка! В данном массиве нет нулевых элементов')
+elif len(negative_nums) == 0:
+    print('Ошибка! В данном массиве нет отрицательных элементов')
+else:
+    max_negative_id = array.index(max(negative_nums))
+    array[max_negative_id], array[last_0_id] = array[last_0_id], array[max_negative_id]
+    for i in range(len(array)):
+        print(f'{i + 1} элемент списка: {array[i]}')

22e603652d73eadb47d3dd25dfc22996cf50aae3
Sergei <igracom@yandex.ru>
2022-10-20 18:41:06 +0300

    u

--- lab6/4.py
+++ lab6/4.py
@@ -8,16 +8,22 @@ while n <= 0:
     print('Ошибка! Количество элементов должно быть больше 0')
     n = int(input('Введите количество элементов списка: '))
 
-array = [0] * n
+array = [0] * n     # наш массив
 
 for i in range(n):
     array[i] = int(input(f'Введите {i + 1} элемент списка: '))
 
-tmp = []
-max_seq = []
-f = True
+# tmp = []            # временный массив
+# max_seq = []        # максимальная последовательность
+tmp_start = -1        # начало временной последовательности
+tmp_end = -1          # конец временной последовательности
+
+max_seq_start = -1    # начало максимальной последовательности
+max_seq_end = -1      # конец максимальной последовательности
+
+f = True            # флаг пустая ли временная последовательность
 for i in range(n):
-    flag = True
+    flag = True     # флаг на простоту числа
 
     for j in range(2, array[i] // 2 + 1):
         if array[i] % j == 0:
@@ -26,26 +32,32 @@ for i in range(n):
 
     if f and flag:
         f = False
-        tmp.append(array[i])
+        # tmp.append(array[i])
+        tmp_start = i
     else:
         if array[i - 1] < array[i] and flag:
-            tmp.append(array[i])
-            if len(tmp) > len(max_seq):
-                max_seq = tmp
+            # tmp.append(array[i])
+            tmp_end = i
+            if (tmp_end - tmp_start + 1) > (max_seq_end - max_seq_start + 1):
+                max_seq_start, max_seq_end = tmp_start, tmp_end
         else:
             if flag:
-                tmp = [array[i]]
+                # tmp = [array[i]]
+                tmp_start = i
             else:
                 f = True
-                tmp = []
+                # tmp = []
+                tmp_start = tmp_end = -1
 
-if len(tmp) > len(max_seq):
-    max_seq = tmp
+# if len(tmp) > len(max_seq):
+#     max_seq = tmp
+if (tmp_end - tmp_start + 1) > (max_seq_end - max_seq_start + 1):
+    max_seq_start, max_seq_end = tmp_start, tmp_end
 
-if len(max_seq) == 0:
+if (max_seq_end - max_seq_start + 1) == 0:
     print('Последовательность с данными критериями не найдена')
 else:
-    print(f'Длинна наиболее длинной последовательности: {len(max_seq)}')
+    print(f'Длинна наиболее длинной последовательности: {(max_seq_end - max_seq_start + 1)}')
     print('Элементы найденной последовательности:')
-    for i in range(len(max_seq)):
-        print(f'{i+1} элемент: {max_seq[i]}')
+    for i in range(max_seq_start, max_seq_end+1):
+        print(f'{i+1} элемент: {array[i]}')
--- lab6/5.py
+++ lab6/5.py
@@ -7,26 +7,32 @@ while n <= 0:
     print('Ошибка! Количество элементов должно быть больше 0')
     n = int(input('Введите количество элементов списка: '))
 
-array = [0] * n
+array = [0] * n             # наш массив
 
-negative_nums = []
-last_0_id = -1
+# negative_nums = []          # все отрицательные числа
+max_negative_id = -1
+last_0_id = -1              # id последнего нулевого
 
 for i in range(n):
     array[i] = int(input(f'Введите {i + 1} элемент списка: '))
 
+    # if array[i] < 0:
+    #     negative_nums.append(array[i])
     if array[i] < 0:
-        negative_nums.append(array[i])
+        if max_negative_id == -1:
+            max_negative_id = i
+        elif array[max_negative_id] < array[i]:
+            max_negative_id = i
 
     if array[i] == 0:
         last_0_id = i
 
 if last_0_id == -1:
     print('Ошибка! В данном массиве нет нулевых элементов')
-elif len(negative_nums) == 0:
+elif max_negative_id == -1:
     print('Ошибка! В данном массиве нет отрицательных элементов')
 else:
-    max_negative_id = array.index(max(negative_nums))
+    # max_negative_id = array.index(max(negative_nums))       # id максимального отрицательного
     array[max_negative_id], array[last_0_id] = array[last_0_id], array[max_negative_id]
     for i in range(len(array)):
         print(f'{i + 1} элемент списка: {array[i]}')
